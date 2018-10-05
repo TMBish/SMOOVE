7e7a54340cc4cc0d264f50ddec414091e0d73e23
Tom Bishop <tomwbish@gmail.com>
2018-10-05 10:11:29 +1000

    Move location of get current season

--- utils/api/api-utilities.R
+++ utils/api/api-utilities.R
@@ -70,19 +70,3 @@ submit_request <- function(endpoint, params) {
   return(response)
   
 }
-
-
-
-get_current_season <- function() {
-  
-  current_date = Sys.Date()
-  current_year = lubridate::year(current_date)-1 ### CHANGE THISSSSS
-  
-  if (lubridate::month(current_date) > 6) {
-    dte_string = paste0(current_year, "-", (current_year + 1)-2000)
-  } else {
-    dte_string = paste0(current_year - 1, "-", current_year-2000)
-  }
-  
-  return(dte_string)
-}
--- utils/data-utilities.R
+++ utils/data-utilities.R
@@ -42,6 +42,19 @@ dedupe_player_career_stats = function(career_log) {
 
 }
 
+get_current_season <- function() {
+  
+  current_date = Sys.Date()
+  current_year = lubridate::year(current_date)-1 ### CHANGE THISSSSS
+  
+  if (lubridate::month(current_date) > 6) {
+    dte_string = paste0(current_year, "-", (current_year + 1)-2000)
+  } else {
+    dte_string = paste0(current_year - 1, "-", current_year-2000)
+  }
+  
+  return(dte_string)
+}
 
 
 get_peer_stats <- function(stats_master, player_master, starter_bench = "Starting", plyr_position = "F") {
